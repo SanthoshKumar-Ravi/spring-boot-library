d10bd02b8dab65804c59444f986c44d283e5fcfa
SanthoshKumar-Ravi <krpsanthoshkumar@gmail.com>
2025-06-18 19:55:10 +0300

    Intial commit

---
+++ spring-boot-lib/integer-lib/src/main/java/com/spring/integer/service/IntegerService.java
@@ -0,0 +1,7 @@
+package com.spring.integer.service;
+
+public class IntegerService {
+    public Integer getInteger(){
+        return 1;
+    }
+}
---
+++ spring-boot-lib/string-lib/src/main/java/com/spring/string/service/StringService.java
@@ -0,0 +1,8 @@
+package com.spring.string.service;
+
+public class StringService {
+
+    public String getString(){
+        return "string-lib";
+    }
+}
